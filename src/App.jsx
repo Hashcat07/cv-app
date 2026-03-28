cae18cd83397db2911ea34123732afd0808993fa
RAY <166890725+Hashcat07@users.noreply.github.com>
2026-03-28 13:07:54 +0530

    fix: add data props for correct updation

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,8 @@ export default function App() {
   return (
     <div className="root-container">
       <h1 className="app-title">CV Builder</h1>
-        <EditArea onChange={{education:setEducationData,general:setGeneralData,experience:setExperienceData}}/>
+        <EditArea onChange={{education:setEducationData,general:setGeneralData,experience:setExperienceData}}
+        data={{education:educationData,general:generalData,experience:experienceData}}/>
         <DisplayArea data={{education:educationData,general:generalData,experience:experienceData}}/>
     </div>
   );
